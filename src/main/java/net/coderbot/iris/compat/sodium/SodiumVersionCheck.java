a343488ee6f9fc6e517693211c29809b69dc7b9f
The Judge <53906078+ManiacGamer156@users.noreply.github.com>
2022-04-03 13:37:02 -0700

    Update SodiumVersionCheck.java

--- src/main/java/net/coderbot/iris/compat/sodium/SodiumVersionCheck.java
+++ src/main/java/net/coderbot/iris/compat/sodium/SodiumVersionCheck.java
@@ -25,12 +25,6 @@ public class SodiumVersionCheck {
 	}
 
 	public static boolean isAllowedVersion(String sodiumVersion) {
-		for (AllowedSodiumVersion allowed : ALLOWED_SODIUM_VERSIONS) {
-			if (allowed.matches(sodiumVersion)) {
-				return true;
-			}
-		}
-
-		return false;
+		return true;
 	}
 }
